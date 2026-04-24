8d0974d4df0c9501a004a37e91311b2cd1e32130
Sourabh Kurhade <sourabhskn7.com>
2026-04-24 12:32:07 +0530

    Requirement-8 completed(Data driven Testing)
    -LoginAPIDataDrivenTest
    - CreateJobAPIDataDrivenTest

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -1,5 +1,6 @@
 package com.api.utils;
 
+import java.beans.beancontext.BeanContext;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
@@ -16,7 +17,7 @@ public class CSVReaderUtil {
 		
 	}
 	
-	public static Iterator<UserBean> loadCSV(String pathOFCSVFile)  {
+	public static <T> Iterator <T> loadCSV(String pathOFCSVFile,Class<T> bean)  {
 
 		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(pathOFCSVFile);
 		InputStreamReader isr = new InputStreamReader(is);
@@ -24,14 +25,13 @@ public class CSVReaderUtil {
 
 		// code to map the CSV to POJO class means UserBean
 
-		CsvToBean<UserBean> csvToBean = new CsvToBeanBuilder(csvReader)
-				.withType(UserBean.class)
+		CsvToBean<T> csvToBean = new CsvToBeanBuilder(csvReader)
+				.withType(bean)
 				.withIgnoreEmptyLine(true)
 				.build();
 
-		List<UserBean> userlist = csvToBean.parse();
-		System.out.println(userlist);
-		return userlist.iterator();
+		List<T> list = csvToBean.parse();
+		return list.iterator();
 		
 		
 
